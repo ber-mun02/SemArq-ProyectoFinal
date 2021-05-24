6968094e95b62ef1c3acc2cc65838b30aac6ae0e
ber-mun02 <84328528+ber-mun02@users.noreply.github.com>
2021-05-24 13:57:36 -0500

    Update ALUControl.v

--- ALUControl.v
+++ ALUControl.v
@@ -6,38 +6,49 @@ module ALUControl (
 	output reg [3:0]ALUSelect	//selector de operación en ALU
 );
 
-always @*
-begin
-if (UCon == 2'b00)
-begin
-	case (InData)
-	6'bxxxxxx:
-		ALUSelect = 4'b0010;
-	endcase
-end
+always @* begin
+	if (UCon == 2'b00) begin
+		case (InData)
+			6'bxxxxxx:
+			begin
+				ALUSelect = 4'b0010;
+			end
+		endcase
+	end
 
-if (UCon == 2'b01)
-begin
-	case (InData)
-	6'bxxxxxx:
-		ALUSelect = 0110;
-	endcase
-end
+	if (UCon == 2'b01) begin
+		case (InData)
+			6'bxxxxxx:
+			begin
+				ALUSelect = 0110;
+			end
+		endcase
+	end
 
-if (UCon == 2'b10)
-begin
-	case (InData)
-	6'b100000:
-		ALUSelect = 0010;
-	6'b100010:
-		ALUSelect = 0110;
-	6'b100100:
-		ALUSelect = 0000;
-	6'b100000:
-		ALUSelect = 0001;
-	6'b100000:
-		ALUSelect = 0111;
-	endcase
+	if (UCon == 2'b10) begin
+		case (InData)
+			6'b100000: //add
+			begin
+				ALUSelect = 0010;
+			end
+			6'b100010: //subtract
+			begin
+				ALUSelect = 0110;
+			end
+			6'b100100: //and
+			begin
+				ALUSelect = 0000;
+			end
+			6'b100101: //or
+			begin
+				ALUSelect = 0001;
+			end
+			6'b101010: //slt
+			begin
+				ALUSelect = 0111;
+			end
+		endcase
+	end
 end
-end
+	
 endmodule
