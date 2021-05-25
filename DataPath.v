2f87194804a0819848f48f38abb710cdba4d3992
JMVillalobos708 <84457999+JMVillalobos708@users.noreply.github.com>
2021-05-24 20:29:05 -0500

    Update DataPath.v

--- DataPath.v
+++ DataPath.v
@@ -4,10 +4,11 @@ module DataPathFase1 (
 	input Eclk
 );
 
-wire [31:0]Instruccion, C3, C4, C5, 
-wire [2:0] ALUOp;
+wire [31:0]Instruccion, C0, C1, C3, C4, C5, C9, C12, C13, C14, C18, C21, C22;
+wire [1:0] C15;
+wire[2:0] C16;
 wire [4:0] C2; 
-wire C0, C1, C6, C7, C8 C9;
+wire C6, C7, C8, C10, C11, C17, C23;
 
 assign C1 = C0 + 4;
 assign C5 = C1 + C4;
@@ -26,31 +27,31 @@ InsMemory DUV2(
 
 UnidadControl DUV3(
 	.OP(Instruccion[31:26]),
-	.MemRead(),
-	.Branch(),
+	.MemRead(C20),
+	.Branch(C6),
 	.MemToReg(),
-	.RegWrite(),
-	.ALUSrc(),
-	.RegDst(),
-	.MemToWrite(),
-	.ALUOp()
+	.RegWrite(C17),
+	.ALUSrc(C11),
+	.RegDst(C10),
+	.MemToWrite(C19),
+	.ALUOp(C15)
 );
 
 mux_5 DUV4(
 	.in_0(Instruccion[20:16]),
 	.in_1(Instruccion[15:11]),
-	.regule(RegDst),
+	.regule(C10),
 	.res(C2)
 );
 
 banco DUV5(
-	.RegEn(RegWrite),
+	.RegEn(C17),
 	.ra1(Instruccion[25:21]),
 	.ra2(Instruccion[20:16]),
 	.aw(C2),
-	.dataIn_b, //32b
-	.dr1,
-	.dr2
+	.dataIn_b(C22),
+	.dr1(C14),
+	.dr2(C12)
 );
 
 sign_extend DUV6(
@@ -70,4 +71,39 @@ mux_32 DUV8(
 	.res(C9)
 );
 
+mux_32 DUV9(
+	.in_0(C12),
+	.in_1(C3),
+	.regule(C11),
+	.res(C13)
+);
+
+ALUControl DUV10(
+	.InData(Instruccion[5:0]),
+	.UCon(C15),
+	.ALUSelect(C16)
+);
+
+alu DUV11(
+	.a(C14),
+	.b(C13),
+	.sel(C16),
+	.res(C18),
+	.ZF(C7)
+);
+
+memory DUV12(
+	.MemWrite(C19), 
+	.MemRead(C20),
+	.dir(C18),
+	.dataIn(C12),
+	.dataOut(C21)
+);
+
+mux_32 DUV13(
+	.in_0(C18),
+	.in_1(C21),
+	.regule(C23),
+	.res(C22)
+);
 endmodule
