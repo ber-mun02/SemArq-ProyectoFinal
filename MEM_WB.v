44d7011374186dc87bc386773095d1aa42b597fe
ber-mun02 <84328528+ber-mun02@users.noreply.github.com>
2021-06-08 11:39:34 -0500

    Update MEM_WB.v

--- MEM_WB.v
+++ MEM_WB.v
@@ -3,14 +3,23 @@
 module MEMWB(
 	input clkMEMWB,
 	input [1:0]WB3,
+	input [4:0]fEXMEM,
+	input [31:0]fRD,
+	input [31:0]fAdd,
 	output reg RegWrite, 
-	output reg MemtoReg
+	output reg MemtoReg,
+	output reg [4:0]toAW,
+	output reg [31:0]tMux5_1,
+	output reg [31:0]tMux5_0
 );
 
 always @(posedge clkMEMWB) 
 begin
 	RegWrite = WB3[1];
 	MemtoReg = WB3[0];
+	tMux5_0 = fAdd;
+	tMux5_1 = fRD;
+	toAW = fEXMEM;
 end
 
 endmodule
