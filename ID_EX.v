49244d836ab438f39823809809e67e5645ba89fe
ber-mun02 <84328528+ber-mun02@users.noreply.github.com>
2021-06-14 11:02:24 -0500

    Update ID_EX.v

--- ID_EX.v
+++ ID_EX.v
@@ -11,6 +11,7 @@ module IDEX(
 	input [31:0]fSE,				//tACsl
 	input [4:0]fIns1,				//tMux5_1
 	input [4:0]fIns2,				//tMux5_2
+	input jump_in;
 	output reg[1:0]Wb1,
 	output reg[2:0]Mem1,
 	output reg RegDst,
@@ -22,6 +23,7 @@ module IDEX(
 	output reg[31:0]tACsl,
 	output reg[4:0]tMux5_1,
 	output reg[4:0]tMux5_2
+	output reg jump_out;
 );
 
 always @(posedge clkIDEX) 
@@ -37,6 +39,7 @@ begin
 	tACsl = fSE;
 	tMux5_1	= fIns1;
 	tMux5_2 = fIns2;
+	jump_out = jump_in;
 end
 
 endmodule
