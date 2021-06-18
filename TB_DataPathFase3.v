5d8d15290c36e56d73b42989655daa1c1cb6f6f1
ber-mun02 <84328528+ber-mun02@users.noreply.github.com>
2021-06-17 22:42:58 -0500

    Update TB_DataPathFase3.v

--- TB_DataPathFase3.v
+++ TB_DataPathFase3.v
@@ -1,20 +1,19 @@
 `timescale 1ns/1ns
 
 module TB_DataPathFase3 ();
-    
-reg clk = 1'b0;     
-      
-DataPathFase3 DUV(
-	.Eclk(clk)
-);   
-	
-always @* #100 begin      
-	if (clk == 1'b1) begin
-		clk <= 1'b0;       
-        end
-	else begin
-		clk <= 1'b1;
-	end      
-end
 
+    reg clk;     
+      
+    DataPathFase3 DUV(
+	.Eclk(clk)
+	);   
+	
+    always #100 clk = ~clk;      
+    initial begin
+        
+        clk <= 1;         
+        #300 
+	$stop;    
+	    
+    end
 endmodule
