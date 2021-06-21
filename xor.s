5785667c68dad55fe58dcb1294faca176162df4d
ber-mun02 <84328528+ber-mun02@users.noreply.github.com>
2021-06-20 20:00:31 -0500

    Update xor.s

--- xor.s
+++ xor.s
@@ -1,44 +1,37 @@
 .inicio:
 
 nop
-
-beq $1, $5, 1
+beq $1, $5, 4
 xori $13, $13, 170             #Aplica clave -> H
-j
-
+sw $13, 0($5)
+j 9
 nop
 nop
 nop
-
-beq $2, $5, 1
+beq $2, $5, 4
 xori $20, $20, 170             #Aplica clave -> O
-j 
-
+sw $13, 0($5)
+j 16
 nop
 nop
 nop
-
-beq $3, $5, 1
+beq $3, $5, 4
 xori $17, $17, 170             #Aplica clave -> L
-j 
-
+sw $13, 0($5)
+j 23
 nop
 nop
 nop
-
-beq $4, $5, 1
+beq $3, $5, 4
 xori $6, $6, 170               #Aplica clave -> A
-j 
-
+sw $13, 0($5)
+j 30
 nop
 nop
 nop
-
-addi $5, $5, 1                 #Registro $5 = $5 + 1
-beq $4, $5, 1                  #Si (4 == 1) -> PC + 1
-j 1                            #Salto a instrucción 1
-
+addi $5, $5, 1                 #Suma $5 = $5 + 1
+beq $4, $5, 1
+j 1
 nop
 nop
 nop
-
